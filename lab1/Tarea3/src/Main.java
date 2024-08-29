7fcf5c09702160a66f17b24487549934b8a66b7f
Williams Avila <molinamolinaa54@gmail.com>
2024-08-29 15:18:18 +0200

    tray1

---
+++ lab1/Tarea3/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x= 15;
+        int y = 27;
+
+        int first = (((4* y)/5)-x);
+        int second = (first*first);
+        int resultado = (second + (x*x));
+        System.out.println(resultado);
+    }
+}
